f46cee137e22be26a8d60d3da90f8d6cfe1f1549
ForkKILLET <fork_killet@qq.com>
2021-12-20 20:14:03 +0800

    release: alpha
    feat(move): Enable to move by keyboard (Arrow keys).
    fix(rank): Fix that minutes were doubled.

--- main.js
+++ main.js
@@ -4,28 +4,39 @@ const sen = "①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮"
 
 const $map = $("#map")
 let $tmp
+const $block = (x, y) => $(`span[data-x="${x}"][data-y="${y}"]`)
 
 const $rank = $("#rank")
 
 const delta = [ [ -1, 0 ], [ 1, 0 ], [ 0, -1 ], [ 0, 1 ] ]
+const key_delta = {
+	ArrowLeft: [ -1, 0 ],
+	ArrowRight: [ 1, 0 ],
+	ArrowUp: [ 0, -1 ],
+	ArrowDown: [ 0, 1 ]
+}
+
+const is_xy_invalid = (x, y) => x < 0 || x > 3 || y < 0 || y > 3
 
 let debug = {
-	immediate_win: false
+	immediate_win: false,
+	delete_record: false
 }
 
 const have_won = () => debug.immediate_win || [ ...$map.children ].map($i => $i.innerHTML).join("") === sen + " "
 
-let t_time, t_info, t_0
+let t_time, t_info, t0
 let stt = "start"
 let min, sec
 
 const record_win = () => {
-	sec = (Date.now() - t_0) % 1e6 / 1e3
-	min += ~~ (sec / 60)
-	sec %= 60
+	stt = "win"
+
+	sec = (Date.now() - t0) % 1e6 / 1e3
+	min = ~~ (sec / 60)
+	sec = ~~ (sec % 60 * 1e3) / 1e3
 
 	$info.innerHTML = `<b id="win">Win!</b> in <u>${min}:${sec}</u>`
-	stt = "win"
 	clearInterval(t_time)
 	t_time = undefined
 
@@ -48,11 +59,12 @@ const record_win = () => {
 
 const gen = () => {
 	stt = "play"
+	$info.innerHTML = "<u>0:0</u>"
 	
 	clearInterval(t_time)
 	clearInterval(t_info)
 
-	t_0 = Date.now(), min = 0, sec = 0
+	t0 = Date.now(), min = 0, sec = 0
 	t_time = setInterval(() => {
 		if (stt === "play") $info.innerHTML = `<u>${min}:${sec}</u>`
 		if (stt === "win") return
@@ -74,35 +86,56 @@ const gen = () => {
 		$b.dataset.x = i % 4
 		$b.dataset.y = ~~ (i / 4)
 		$b.innerHTML = chao[i] ?? " "
-		$b.onclick = evt => {
-			if (stt !== "play") return
-			const $cur = evt.currentTarget
-			const x = + $cur.dataset.x
-			const y = + $cur.dataset.y
-			for (const [ dx, dy ] of delta) {
-				const x_ = x + dx
-				const y_ = y + dy
-				if (x_ < 0 || x_ > 3 || y_ < 0 || y_ > 3)
-					continue
-				const $nxt = $(`span[data-x="${x_}"][data-y="${y_}"]`)
-				if ($nxt.id === "b-15") {
-					$nxt.dataset.x = $cur.dataset.x
-					$nxt.dataset.y = $cur.dataset.y
-					$cur.dataset.x = x_
-					$cur.dataset.y = y_
-
-					$map.replaceChild($tmp, $cur)
-					$map.replaceChild($cur, $nxt)
-					$map.replaceChild($nxt, $tmp)
-
-					if (have_won()) record_win()
-
-					break
-				}
-			}
-		}
+		$b.addEventListener("click", move.mouse)
 	}
 }
+
+const move = {
+	do: ($e1, $e2) => {
+		$map.replaceChild($tmp, $e2)
+		$map.replaceChild($e2, $e1)
+		$map.replaceChild($e1, $tmp)
+
+		; [ $e1.dataset.x, $e2.dataset.x ] = [ $e2.dataset.x, $e1.dataset.x ]
+		; [ $e1.dataset.y, $e2.dataset.y ] = [ $e2.dataset.y, $e1.dataset.y ]
+	},
+
+	mouse: evt => {
+		if (stt !== "play") return
+		const $cur = evt.currentTarget
+		const { x, y } = $cur.dataset
+		for (const [ dx, dy ] of delta) {
+			const x_ = + x + dx
+			const y_ = + y + dy
+			if (is_xy_invalid(x_, y_)) continue
+
+			const $nxt = $block(x_, y_)
+			if ($nxt.id === "b-15") {
+				move.do($cur, $nxt)
+				if (have_won()) record_win()
+				break
+			}
+		}
+	},
+
+	keyboard: evt => {
+		if (stt !== "play") return
+		const d = key_delta[evt.key]
+		if (! d) return
+
+		const $emp = $("#b-15")
+		const { x, y } = $emp.dataset
+		const [ dx, dy ] = d
+		const x_ = x - dx
+		const y_ = y - dy
+
+		if (is_xy_invalid(x_, y_)) return
+		move.do($emp, $block(x_, y_))
+		if (have_won()) record_win()
+	}
+}
+
+document.addEventListener("keydown", move.keyboard)
 
 const rank = () => JSON.parse(localStorage.rank ?? "[]")
 rank.mod = cb => {
@@ -156,9 +189,13 @@ const stt_op = {
 		})
 	}
 }
+const exe_op = evt => stt_op[stt](evt)
 
 const $info = $("#info")
-$info.onclick = evt => stt_op[stt](evt)
+$info.addEventListener("click", exe_op)
+document.addEventListener("keydown", evt => {
+	if (evt.key === "Enter") exe_op(evt)
+})
 	
 rank().forEach(([ name, min, sec ]) => {
 	const $l = document.createElement("li")
